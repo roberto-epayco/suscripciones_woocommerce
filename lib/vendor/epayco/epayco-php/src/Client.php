cad44667abcef118c3ff000148ef33e89ce0c4cb
pruebasepayco <ricardo.saldarriaga@payco.co>
2020-07-29 16:20:53 -0500

    configurated client.php file

--- lib/vendor/epayco/epayco-php/src/Client.php
+++ lib/vendor/epayco/epayco-php/src/Client.php
@@ -15,20 +15,18 @@ class Client extends GraphqlClient
 
     const BASE_URL = "https://api.secure.payco.co";
     const BASE_URL_SECURE = "https://secure.payco.co";
-    // const BASE_URL = "https://apitest.secure.epayco.io";
-    // const BASE_URL_SECURE = "https://secure.epayco.io";
     const IV = "0000000000000000";
     const LENGUAGE = "php";
 
     /**
      * Request api epayco
-     * @param  String $method      method petition
-     * @param  String $url         url request api epayco
-     * @param  String $api_key     public key commerce
-     * @param  Object $data        data petition
-     * @param  String $private_key private key commerce
-     * @param  String $test        type petition production or testing
-     * @param  Boolean $switch     type api petition
+     * @param String $method method petition
+     * @param String $url url request api epayco
+     * @param String $api_key public key commerce
+     * @param Object $data data petition
+     * @param String $private_key private key commerce
+     * @param String $test type petition production or testing
+     * @param Boolean $switch type api petition
      * @return Object
      */
     public function request(
@@ -43,7 +41,8 @@ class Client extends GraphqlClient
         $cash = null,
         $safetyp = null,
         $card = null
-    ) {
+    )
+    {
 
         /**
          * Resources ip, traslate keys
@@ -55,40 +54,66 @@ class Client extends GraphqlClient
          */
         if ($switch && is_array($data)) {
             if ($safetyp) {
-             $data = $util->setKeys($data, $safetyp);
-            }else{
-            $data = $util->setKeys($data);
-          }
+                $data = $util->setKeys($data, $safetyp);
+            } else {
+                $data = $util->setKeys($data);
+            }
         }
-//42677847
-        
-        /**
-         * Set heaToken bearer
-         */
-   //    $dataAuth =$this->authentication($api_key,$private_key);
-   //  $auth=gettype($dataAuth);
-   // $json = json_decode($dataAuth);
-   // $bearer_token=$json->bearer_token;
+        try {
+            /**
+             * Set heaToken bearer
+             */
+         
+        if(!isset($_COOKIE[$api_key])) {
+            //  echo "Cookie named '" . $cookie_name . "' is not set!";
+              $dataAuth =$this->authentication($api_key,$private_key);
+              $auth=gettype($dataAuth);
+              $json = json_decode($dataAuth);
+              if(!is_object($json)) {
+                  throw new ErrorException("Error get bearer_token.", 106);
+              }
+              if(!$json->status)
+              {
+                  throw new ErrorException($json->message);
+              }
+              $bearer_token=$json->bearer_token;
+              $cookie_name = $api_key;
+              $cookie_value = $bearer_token;
+              setcookie($cookie_name, $cookie_value, time() + (60 * 14), "/"); 
+            //  echo "token con login".$bearer_token;
+              }else{
+                $bearer_token = $_COOKIE[$api_key];
+             //   echo "token sin login".$bearer_token;
+              } 
 
-        /**
-         * Set headers
-         */
-         // $headers= array("Content-Type" => "application/json","Accept" => "application/json","Type"=>'sdk-jwt',"Authorization"=>'Bearer '.$bearer_token, "lang"=>"PHP" );
-         $headers= array("Content-Type" => "application/json","Accept" => "application/json","Type"=>'sdk', "lang"=>"PHP" );
+        } catch (\Exception $e) {
+            $data = [
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ];
+            $objectReturnError = (object)$data;
+            return $objectReturnError;
+        }
 
         try {
+
+            /**
+             * Set headers
+             */
+            $headers = array("Content-Type" => "application/json", "Accept" => "application/json", "Type" => 'sdk-jwt', "Authorization" => 'Bearer ' . $bearer_token, "lang" => "PHP");
+
             $options = array(
-              'auth' => new \Requests_Auth_Basic(array($api_key, '')),
                 'timeout' => 120,
                 'connect_timeout' => 120,
             );
 
             if ($method == "GET") {
                 if ($switch) {
-                    if($test){
-                        $test="TRUE";
-                    }else{
-                        $test="FALSE";
+                    if ($test) {
+                        $test = "TRUE";
+                    } else {
+                        $test = "FALSE";
                     }
 
                     $response = \Requests::get(Client::BASE_URL_SECURE . $url, $headers, $options);
@@ -98,67 +123,70 @@ class Client extends GraphqlClient
             } elseif ($method == "POST") {
 
                 if ($switch) {
-                    $data = $util->mergeSet($data, $test, $lang, $private_key, $api_key , $cash);
+                    $data = $util->mergeSet($data, $test, $lang, $private_key, $api_key, $cash);
 
                     $response = \Requests::post(Client::BASE_URL_SECURE . $url, $headers, json_encode($data), $options);
                 } else {
- 
+
                     if ($card) {
-         //    var_dump(Client::BASE_URL_SECURE . $url,json_encode($data),$headers);
-         // die();
-                          $response = \Requests::post(Client::BASE_URL . $url, $headers, json_encode($data), $options);
-                    }else{
-       
-                  $data["ip"] = isset($data["ip"]) ? $data["ip"] : getHostByName(getHostName());
-                    $data["test"] = $test;
-                  
-                     $response = \Requests::post(Client::BASE_URL . $url, $headers, json_encode($data), $options);
+
+                        $response = \Requests::post(Client::BASE_URL . $url, $headers, json_encode($data), $options);
+                    } else {
+
+                        $data["ip"] = isset($data["ip"]) ? $data["ip"] : getHostByName(getHostName());
+                        $data["test"] = $test;
+
+                        $response = \Requests::post(Client::BASE_URL . $url, $headers, json_encode($data), $options);
                     }
-  
+
                 }
                 if ($safetyp) {
-                    $headers2= array( "Accept" => "multipart/form-data");
-                    $data = $util->mergeSet($data, $test, $lang, $private_key, $api_key , $cash);
-                         $response = \Requests::post(Client::BASE_URL_SECURE . $url, $headers2,$data, $options);
-                      
+                    $headers2 = array("Accept" => "multipart/form-data");
+                    $data = $util->mergeSet($data, $test, $lang, $private_key, $api_key, $cash);
+                    $response = \Requests::post(Client::BASE_URL_SECURE . $url, $headers2, $data, $options);
+
                 }
             } elseif ($method == "DELETE") {
                 $response = \Requests::delete(Client::BASE_URL . $url, $headers, $options);
             }
         } catch (\Exception $e) {
-            throw new ErrorException($e->getMessage(), 101);
+            throw new ErrorException($e->getMessage(), $e->getCode());
         }
-        if ($response->status_code >= 200 && $response->status_code <= 206) {
-            if ($method == "DELETE") {
-                return $response->status_code == 204 || $response->status_code == 200;
+        try {
+            if ($response->status_code >= 200 && $response->status_code <= 206) {
+                if ($method == "DELETE") {
+                    return $response->status_code == 204 || $response->status_code == 200;
+                }
+                return json_decode($response->body);
             }
-            return json_decode($response->body);
-        }
-        if ($response->status_code == 400) {
-            $code = 0;
-            $message = "";
-            try {
-                $error = (array) json_decode($response->body)->errors[0];
-                $code = key($error);
-                $message = current($error);
-            } catch (\Exception $e) {
-                throw new ErrorException($lang, 102);
+            if ($response->status_code == 400) {
+                $code = 0;
+                $message = "Bad request";
+                try {
+                    $error = (array)json_decode($response->body)->errors[0];
+                    $code = key($error);
+                    $message = current($error);
+                } catch (\Exception $e) {
+                    throw new ErrorException($e->getMessage(), $e->getCode());
+                }
+                throw new ErrorException($message , 103);
             }
-            throw new ErrorException($lang, 103);
+            if ($response->status_code == 401) {
+                throw new ErrorException('Unauthorized', 104);
+            }
+            if ($response->status_code == 404) {
+                throw new ErrorException('Not found', 105);
+            }
+            if ($response->status_code == 403) {
+                throw new ErrorException('Permission denegated', 106);
+            }
+            if ($response->status_code == 405) {
+                throw new ErrorException('Not allowed', 107);
+            }
+            throw new ErrorException('Internal error', 102);
+        } catch (\Exception $e) {
+            throw new ErrorException($e->getMessage(), $e->getCode() );
         }
-        if ($response->status_code == 401) {
-            throw new ErrorException($lang, 104);
-        }
-        if ($response->status_code == 404) {
-            throw new ErrorException($lang, 105);
-        }
-        if ($response->status_code == 403) {
-            throw new ErrorException($lang, 106);
-        }
-        if ($response->status_code == 405) {
-            throw new ErrorException($lang, 107);
-        }
-        throw new ErrorException($lang, 102);
     }
 
     public function graphql(
@@ -168,14 +196,14 @@ class Client extends GraphqlClient
         $type,
         $custom_key)
     {
-        try{
+        try {
             $queryString = "";
             $initial_key = "";
-            switch ($type){
+            switch ($type) {
                 case "wrapper":
                     $this->validate($query); //query validator
-                    $schema = $query->action === "find" ? $schema."s": $schema;
-                    $this->canPaginateSchema($query->action,$query->pagination,$schema);
+                    $schema = $query->action === "find" ? $schema . "s" : $schema;
+                    $this->canPaginateSchema($query->action, $query->pagination, $schema);
                     $selectorParams = $this->paramsBuilder($query);
 
                     $queryString = $this->queryString(
@@ -189,45 +217,33 @@ class Client extends GraphqlClient
                     $initial_key = $custom_key;
                     break;
             }
-            $result = $this->sendRequest($queryString,$api_key);
-            return $this->successResponse($result,$initial_key);
-        }catch (\Exception $e){
+            $result = $this->sendRequest($queryString, $api_key);
+            return $this->successResponse($result, $initial_key);
+        } catch (\Exception $e) {
             throw new ErrorException($e->getMessage(), 301);
         }
 
     }
 
-    public function authentication($api_key,$private_key)
-    {  
+    public function authentication($api_key, $private_key)
+    {
         $data = array(
-                'public_key' => $api_key,
-                'private_key' => $private_key
-            );
-         $json=  json_encode($data);
-$curl = curl_init();
+            'public_key' => $api_key,
+            'private_key' => $private_key
+        );
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.secure.payco.co/v1/auth/login",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS =>$json,
-  CURLOPT_HTTPHEADER => array(
-    "Content-Type: application/json",
-    "type: sdk-jwt",
-    "Accept: application/json"
-  ),
-));
+        $headers = array("Content-Type" => "application/json", "Accept" => "application/json", "Type" => 'sdk-jwt', "lang" => "PHP");
 
-$response = curl_exec($curl);
+        $options = array(
+            'timeout' => 120,
+            'connect_timeout' => 120,
+        );
 
-curl_close($curl);
+        $url = "/v1/auth/login";
 
-         return $response;
+        $response = \Requests::post(Client::BASE_URL . $url, $headers, json_encode($data), $options);
+
+
+        return isset($response->body) ? $response->body : false;
     }
 }
-
